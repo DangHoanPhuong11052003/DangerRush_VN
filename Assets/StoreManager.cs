683c3d48a97d8065b76895becf00fbc90e2a8211
Đặng Hoàn Phương <danghoanphuong11052003@gmail.com>
2024-09-05 21:35:06 +0700

    Create Store UI

---
+++ Assets/StoreManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StoreManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI fishBoneValue;
+
+    private GameData gameData;
+
+    private void Start()
+    {
+        gameData = LocalData.instance.GetGameData();
+
+        fishBoneValue.text = gameData.coin.ToString().Length<9? gameData.coin.ToString(): gameData.coin.ToString().Substring(10)+"...";
+    }
+}
--- Assets/UIMainManager.cs
+++ Assets/UIMainManager.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class UIMainManager : MonoBehaviour
 {
+    [SerializeField] private GameObject Store;
     public void LoadGame()
     {
         GameManager.instance.LoadGame();
+    }
+
+    public void OpenOrCloseStore(bool isOpen)
+    {
+        Store.SetActive(isOpen);
     }
 }
