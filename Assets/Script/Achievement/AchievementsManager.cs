37a93bbedbae74ac7d817f0fcecac41cb2e92d9e
Đặng Hoàn Phương <danghoanphuong11052003@gmail.com>
2024-11-07 00:21:51 +0700

    demo show achievement notifi

---
+++ Assets/Script/Achievement/AchievementsManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementsManager : MonoBehaviour
+{
+    [SerializeField] private AchivementData achivementData;
+    [SerializeField] private AchievementsNotification notification;
+    [SerializeField] private List<string> notificationLst = new List<string>();
+    [SerializeField] private Animator notificationAnimator;
+
+    public static AchievementsManager instance;
+    private float timer;
+    private float timeDelayNotifi;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        timeDelayNotifi = notificationAnimator.runtimeAnimatorController.animationClips[0].length+0.5f;
+
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            notificationLst.Add("agagaa " + i);
+        }
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (notificationLst.Count > 0&& timer <= 0)
+        {
+            ShowAchievementsNotification(notificationLst[0]);
+            notificationLst.RemoveAt(0);
+
+            timer = timeDelayNotifi;
+        }
+    }
+
+    private void ShowAchievementsNotification(string title)
+    {
+        notification.gameObject.SetActive(true);
+        notification.ShowAchievementsNotification(title);
+    }
+
+
+}
---
+++ Assets/Script/Achievement/AchievementsNotification.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AchievementsNotification : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI title;
+    [SerializeField] private Animator animator;
+
+    public void ShowAchievementsNotification(string title)
+    {
+        this.title.text = title;
+
+        animator.SetTrigger("isActive");
+        float timeAnimation = animator.runtimeAnimatorController.animationClips[0].length;
+
+        StartCoroutine(DeActiveAfterAnimation(timeAnimation));
+    }
+
+    private IEnumerator DeActiveAfterAnimation(float time)
+    {
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(false);
+    }
+}
